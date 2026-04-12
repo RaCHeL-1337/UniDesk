43c9ee2c2373d5444ead546e13dadf864dabb156
Yauheni Misiuk <evgehapvp8@gmail.com>
2026-04-12 03:16:22 +0200

    LAB6: fixed TicketsController by adding Create GET and POST actions

--- src/UniDesk.Web/Controllers/TicketsController.cs
+++ src/UniDesk.Web/Controllers/TicketsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using UniDesk.Web.Services;
 using UniDesk.Web.DTOs;
+using UniDesk.Web.Models;
+using UniDesk.Web.Services;
 
 namespace UniDesk.Web.Controllers
 {
@@ -21,6 +22,27 @@ namespace UniDesk.Web.Controllers
             ViewBag.TotalCount = result.TotalCount;
 
             return View(result.Items);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(CreateTicketRequest request)
+        {
+            if (!ModelState.IsValid)
+                return View(request);
+
+            var ticket = new Ticket
+            {
+                Title = request.Title,
+                Description = request.Description
+            };
+
+            _ticketService.Add(ticket);
+
+            return RedirectToAction("Index");
         }
     }
 }
